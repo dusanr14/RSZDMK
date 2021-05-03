8973f469e5c29cebc07242f42661ca7a28397455
Your Name <dusanr14@gmail.com>
2021-05-03 19:08:05 +0200

    PrintResult function added

--- vezba1/zadatak1/main.c
+++ vezba1/zadatak1/main.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-int main()
+void PrintResult(int res)
 {
-int a = 3;
-int b = 8;
-int c;
-if(a > b)
-c = a - b;
-else
-c = b - a;
-printf("Result: %d\n", c);
-return 0;
+printf("Result: %d\n", res);
 }
